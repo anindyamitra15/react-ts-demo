36c4397be0f654d899925a70d56d9b2cb5aabfa9
Anindya Mitra <anindya15l12@gmail.com>
2022-06-25 16:34:54 +0530

    added useful comments

--- src/App.tsx
+++ src/App.tsx
@@ -2,19 +2,21 @@ import { FC, useEffect, useState } from 'react';
 import './App.css';
 
 const App: FC = () => {
+  // "counter" is a number state initialised to 0, "setCounter" is a function which sets "counter'"
   const [counter, setCounter] = useState<number>(0);
 
   useEffect(() => {
 
-    const timeout = setTimeout(() => {
+    const timeout = setTimeout(() => { //this part is run
       console.log("Reacting to counter after 2 s");
     }, 2000);
 
     return () => {
+      // cleanup code (run during component destruction)
       clearTimeout(timeout);
     };
 
-  }, [counter]);
+  }, [counter]); //changing "counter" triggers this useEffect
 
 
   return (
