f794dec6e9b69968fa973fd6a774f8d818af810c
Anindya Mitra <anindya15l12@gmail.com>
2022-06-25 16:20:01 +0530

    useEffect working

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,27 @@
-import { FC, useState } from 'react';
+import { FC, useEffect, useState } from 'react';
 import './App.css';
 
 const App: FC = () => {
   const [counter, setCounter] = useState<number>(0);
+
+  useEffect(() => {
+
+    const timeout = setTimeout(() => {
+      console.log("Reacting to counter after 2 s");
+    }, 2000);
+
+    return () => {
+      clearTimeout(timeout);
+    };
+
+  }, [counter]);
+
+
   return (
     <div className="App">
       <p className="purpose">This repository will have various react hooks and redux experimented on it</p>
-      <p>This is the state: {counter}</p>
-      <p>Click to change the state:&nbsp;
+      <p>This is the counter state: {counter}</p>
+      <p>Click to change the state of counter:&nbsp;
         <button onClick={() => setCounter(counter - 1)}>dec</button>&nbsp;
         <button onClick={() => setCounter(counter + 1)}>inc</button>
       </p>
