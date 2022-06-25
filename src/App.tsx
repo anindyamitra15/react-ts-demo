cad528b66008c9a94c1c7d9d1cb4d244b7b6b4a7
Anindya Mitra <anindya15l12@gmail.com>
2022-06-25 16:10:31 +0530

    useState demo

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,16 @@
-import { FC } from 'react';
+import { FC, useState } from 'react';
 import './App.css';
 
 const App: FC = () => {
+  const [counter, setCounter] = useState<number>(0);
   return (
     <div className="App">
       <p className="purpose">This repository will have various react hooks and redux experimented on it</p>
+      <p>This is the state: {counter}</p>
+      <p>Click to change the state:&nbsp;
+        <button onClick={() => setCounter(counter - 1)}>dec</button>&nbsp;
+        <button onClick={() => setCounter(counter + 1)}>inc</button>
+      </p>
     </div>
   );
 }
